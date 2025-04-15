8e610b1a621653c055805fe414a82aa94075b059
Alves56 <126252599+Alves56@users.noreply.github.com>
2025-04-14 22:14:22 -0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.teste1;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.teste1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.teste);
+        TextView t1 = findViewById(R.id.Texto1);
+        TextView t2 = findViewById(R.id.Texto2);
+        TextView t3 = findViewById(R.id.Texto3);
+
+        t1.setText("macaco");
+        t2.setText("gosta");
+        t3.setText("banana");
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
